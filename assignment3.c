fed16783b5fadaf4ac5027aa1bfb96116ad3cefe
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-01-31 05:16:36 +0000

    assignment3.c write commit

--- assignment3.c
+++ assignment3.c
@@ -1,1 +1,11 @@
-//Enter your code here
+#include<stdio.h>
+int main()
+{
+	float a,b,c,sum,avg;
+	printf("Enter the 3 numbers\n");
+	scanf("%f%f%f",&a,&b,&c);
+	sum=a+b+c;
+	avg=sum/3;
+	printf("The Sum and Average are : %f %f \n",sum,avg);
+	return 0;
+}
