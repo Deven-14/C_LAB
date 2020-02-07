3c9f4c8607125f271f374fc6ab2b067694c3ec66
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-07 04:39:48 +0000

    test1.c write commit

--- test1.c
+++ test1.c
@@ -1,1 +1,10 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	float p,t,r,si;
+	printf("Enter the Principal, Time and Rate of interest\n");
+	scanf("%f%f%f",&p,&t,&r);
+	si=(p*t*r)/100;
+	printf("The Simple Interest is %f\n",si);
+	return 0;
+}
