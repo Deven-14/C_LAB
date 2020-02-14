5bec6e41fd15a50646feca343d8a6d9900dc4ee0
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-14 04:30:37 +0000

    test2.c final commit

--- test2.c
+++ test2.c
@@ -1,1 +1,25 @@
-//write your code here
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	float r1,r2,d;
+	printf("Enter the values of a, b and c \n");
+	scanf("%d%d%d",&a,&b,&c);
+	d=b*b-4*a*c;
+	if(d>0)
+	{
+		r1=(-b+sqrt(d))/(2*a);
+		r2=(-b-sqrt(d))/(2*a);
+		printf("The roots are real and distinct. \nThe roots are %f and %f. \n",r1,r2);
+	}
+	else if(d==0)
+	{
+		r1=-b/(2*a);
+		r2=r1;
+		printf("The roots are real and equal. \nThe roots are %f and %f. \n",r1,r2);
+	}
+	else
+		printf("Roots are imaginary\n");
+	return 0;
+}
