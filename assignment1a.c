7e5934dc0b045d62f11cfdd719716ef86601e667
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-14 04:56:22 +0000

    assignment1a.c final commit

--- assignment1a.c
+++ assignment1a.c
@@ -4,7 +4,7 @@ int main()
     int a,b,c,l;
     printf("Enter three numbers\n");
     scanf("%d%d%d",&a,&b,&c);
-    l=((a>=b)?((a>=c)a:c):((b>=c)b:c);
-    printf("The largest number is %d",l);
+    l=(a>=b)?((a>=c)?a:c):((b>=c)?b:c);
+    printf("The largest number is %d\n",l);
     return 0;
 }
