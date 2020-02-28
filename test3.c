50d7c2231388a871d530c48bdbe97bdece73a7bd
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-28 04:04:30 +0000

    test3.c initial commit

--- test3.c
+++ test3.c
@@ -1,1 +1,26 @@
-//write your code here
+#include<stdio.h>
+#define pi 3.14
+float input()
+{
+	float r;
+	printf("Enter the radius of a circle\n");
+	scanf("%f",&r);
+	return r;
+}
+float process(float r)
+{
+	float area=pi*r*r;
+	return r;
+}
+void output(float area)
+{
+	printf("The area of the circl is %f",area);
+}
+int main()
+{ 
+	float r, area;
+	r=input();
+	area=prcess(r);
+	output(area);
+	return 0;
+}
