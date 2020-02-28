7cc41520ad044a3c5fa1cda82bb367580996c469
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-28 05:25:25 +0000

    Assignment2a.c modified commit

--- Assignment2a.c
+++ Assignment2a.c
@@ -4,22 +4,22 @@ int main()
     float s,b,t;
     char ch;
     printf("Enter the salary and gender (m/f) of the employee\n");
-    scanf("%f%c",&s,&ch);
+    scanf("%f %c",&s,&ch);
     if(ch=='m')
     {
         if(s<10000)
-            b=s*(7/100);
+            b=s*0.07;
         else 
-            b=s*(5/100);
+            b=s*0.05;
     }
     else
     {
         if(s<10000)
-            b=s*(12/100);
+            b=s*0.12;
         else
-            b=s*(10/100);
+            b=s*0.10;
     }
     t=s+b;
-    printf("The salary to be received with bonus is %f",t);
+    printf("The salary to be received with bonus is %f\n",t);
     return 0;
 }
