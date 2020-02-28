cfc5953474a7d9da1d49bcd41d8960c6c977a9e1
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-02-28 03:53:00 +0000

    lab3.c initial commit

--- lab3.c
+++ lab3.c
@@ -1,1 +1,27 @@
-//write your code here
+#include<stdio.h>
+int input()
+{
+	int a;
+	scanf("%d",&a);
+	return a;
+}
+int process(int h,int m)
+{
+	int d=h*60+m;
+	return d;
+}
+void output(int h,int m,int d)
+{
+	printf("%d Hours and %d mintues = %d mintues\n",h,m,d);
+}
+int main()
+{
+	int h,m,d;
+	printf("Enter the number of hours\n");
+	h=input();
+	printf("Enter the number of mintues\n");
+	m=input();
+	d=process(h,m);
+	output(h,m,d);
+	return 0;
+}
