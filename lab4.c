401a3a4cd8d11bba29f0dcce84dd6d6028a22f32
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-06 03:48:32 +0000

    lab4.c initial commit

--- lab4.c
+++ lab4.c
@@ -1,1 +1,16 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int m,n,sum=0;
+	printf("Enter a number\n");
+	scanf("%d",&n);
+	m=n;
+	while(n>0)
+	{
+		n=n%10;
+		sum+=n;
+		n=n/10;
+	}
+	printf("The sum of the digits of %d is %d\n",m,sum);
+	return 0;
+}
