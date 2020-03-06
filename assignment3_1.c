ab0d03543aedcd16411e910b5c7b3ee81c309ec4
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-06 04:39:45 +0000

    assignment3_1.c initial commit

---
+++ assignment3_1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,flag;
+    float r1,r2,d;
+    printf("Enter the coefficients of a quadrtic equation\n");
+    scanf("%d%d%d",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d>0)
+        flag=1;
+    else if(d==0)
+        flag=2;
+    switch(flag)
+    {
+        case 1:
+            r1=(-b+srqt(d))/(2*a);
+            r2=(-b-srqt(d))/(2*a);
+            printf("The roots are real and distinct.\n");
+            printf("The roots are %f and %f\n",r1,r2);
+            break;
+        case 2:
+            r1=(-b)/(2*a);
+            r2=r1;
+            printf("The roots are real and equal.\n");
+            printf("The roots are %f and %f\n",r1,r2);
+            break;
+        default:printf("The roots are imaginary");
+    }
+    return 0;
+}
