18c25bc009865edb263f500c6c79888967b446d8
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-06 04:20:52 +0000

    test5.c initial commit

--- test5.c
+++ test5.c
@@ -1,1 +1,14 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter a number:\n");
+	scanf("%d",&n);
+	printf("The multiples of %d are ",n);
+	for(i=1;i<=100;i++)
+	{
+		if(i%n==0)
+			printf("%d, ",i);
+	}
+	return 0;
+}
