8c0259885b09b4bee6fb9ccba9b6d03c922ff297
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-06 04:51:46 +0000

    assignment3_2a.c modified commit

--- assignment3_2a.c
+++ assignment3_2a.c
@@ -4,8 +4,9 @@ int main()
     int i,j;
         for(i=1;i<=4;i++)
         {
-            for(j=1;j<i;j++)
+            for(j=1;j<=i;j++)
                 printf("%d",j);
+            printf("\n");
         }
     return 0;
 }
