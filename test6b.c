907d911fa3af71ab9be12594bc7640f30529dbc1
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-13 04:40:40 +0000

    test6b.c final commit 1

--- test6b.c
+++ test6b.c
@@ -2,10 +2,12 @@
 int main()
 {
     int sum=0,i,n;
+    float avg;
     printf("Enter the value of n:\n");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
         sum+=i;
-    printf("The sum of first %d numbers is: %d\n",n,sum);
+    avg=(float)sum/n;
+    printf("The avg of first %d numbers is: %f\n",n,avg);
     return 0;
 }
