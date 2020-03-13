c65726b99c0c896bfde811b4355289470c2a84dc
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-13 04:21:51 +0000

    test6a.c final commit

--- test6a.c
+++ test6a.c
@@ -2,13 +2,12 @@
 int main()
 {
     int sum=0,n,i,a[50];
-    printf("Enter the value of n\n");
+    printf("Enter the value of n:\n");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
         a[i]=i;
     for(i=1;i<=n;i++)
         sum+=a[i];
-    printf("The sum of %d numbers is: %d\n",n,sum);
+    printf("The sum of first %d numbers is: %d\n",n,sum);
     return 0;
 }
-
