7960fbd618532ff1bc0d3ae956662ab529aac1db
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-13 04:45:39 +0000

    assignment4a.c initial commit

---
+++ assignment4a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int a,i,fact=1;
+ printf("Enter an interger:\n");
+ scanf("%d",&a);
+ for(i=1;i<=a;i++)
+ fact=fact*i;
+ printf("The factorial of %d! is %d\n",a,fact);
+ return 0;
+}
