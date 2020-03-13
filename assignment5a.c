4d8d6646a310dbbf7027dcde0842aaf1459f9951
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-13 05:14:38 +0000

    assignment5a.c final commit

--- assignment5a.c
+++ assignment5a.c
@@ -5,10 +5,10 @@ int main()
     printf("The array before storing the missing element 10 is:\n");
     for(i=0;i<7;i++)
         printf("%d ",even[i]);
-    for(i=4;i<7;i++)
-        a[i+1]=a[i];
-    a[4]=10;
-    printf("The array after storing the missing element 10 is:\n");
+    for(i=6;i>=4;i--)   //i=n-1;i<=pos;i--
+        even[i+1]=even[i];
+    even[4]=10;
+    printf("\nThe array after storing the missing element 10 is:\n");
     for(i=0;i<8;i++)
         printf("%d ",even[i]);
     return 0;
