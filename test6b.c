34725fd40cbd1eaabe8fa8f95183eb839b85047c
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-13 04:25:50 +0000

    test6b.c initial commit

---
+++ test6b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int ,sum=0,i,n;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        sum+=i;
+    printf("The sum of first %d numbers is: %d\n",n,sum);
+    return 0;
+}
