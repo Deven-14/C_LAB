84e6495e809452adb3bf73b3e15794e28064dc55
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-21 14:49:39 +0000

    assignment6a.c modified commit

--- assignment6a.c
+++ assignment6a.c
@@ -7,6 +7,6 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
         sum+=pow(2*i,2);
-    printf("The sum of the series 2^2+4^2+...+%d^2=%d",n,sum);
+    printf("The sum of the series 2^2+4^2+...+%d^2=%d",2*n,sum);
     return 0;
 }
