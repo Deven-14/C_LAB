307e2da74030a8d101c2b1048b2de318a1169862
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-21 14:59:48 +0000

    assignment6b.c initial commit

---
+++ assignment6b.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,n;
+    float sum=0;
+    printf("Enter the value of n:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        sum+=(float)(1/pow(i,2));
+    printf("The sum of the series 1/1^2 +1/2^2 +....+1/%d^2 =%0.2f",n,sum);
+    return 0;
+}
