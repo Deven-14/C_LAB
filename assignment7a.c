9cd82839b778371615a49cc9e143c3b497ee880f
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-24 16:30:17 +0000

    assignment7a.c initial commit

---
+++ assignment7a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[6]={2,3,5,7,11,13},s=11,i;
+    for(i=0;i<6;i++)
+    { 
+        if(a[i]==s)
+        {
+            printf(" 11 is present in the position %d\n",i);
+            break;
+        }
+    }
+    if(i==n)
+        printf("11 is not present in the array\n");
+    return 0;
+}
