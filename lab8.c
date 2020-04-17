acf5f8155635aedecbd85ac64827eac29d2552eb
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-17 09:12:38 +0000

    lab8.c initial commit

--- lab8.c
+++ lab8.c
@@ -1,1 +1,30 @@
-//write your code here
+#include<stdio.h>
+struct employee
+{
+	int eno;
+	char ename[50];
+	float salary;
+	char DOB[80];
+	char companyname[80];
+};
+int main()
+{
+	struct employee e;
+	printf("Enter the employee number:\n");
+	scanf("%d",&e.eno);
+	printf("Enter the employee name:\n");
+	scanf("%s",e.ename);
+	printf("Enter the salary:\n");
+	scanf("%f",&e.salary);
+	printf("Enter the DOB:\n");
+	scanf("%s",e.DOB);
+	printf("Enter the company name:\n");
+	scanf("%s",e.companyname);
+	printf("Employee details are:\n");
+	printf("Employee number =%d\n",e.eno);
+	printf("Employee name=%s\n",e.ename);
+	printf("Employee salary=%f\n",e.salary);
+	printf("Employee DOB=%s\n",e.DOB);
+	printf("Company name=%s\n",e.companyname);
+	return 0;
+}
