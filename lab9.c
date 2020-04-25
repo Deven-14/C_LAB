78635f3e273e7e70699e667305d5afd23e374f38
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-25 20:13:37 +0000

    lab9.c  initial commit

--- lab9.c
+++ lab9.c
@@ -1,1 +1,21 @@
-//write your code here
+#include<stdio.h>
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+int main()
+{
+	int num1,num2;
+	printf("Enter 2 integers:\n");
+	scanf("%d%d",&num1,&num2);
+	printf("Before swapping:\n");
+	printf("The value of number 1=%d\n",num1);
+	printf("The value of number 2=%d\n",num2);
+	swap(&num1,&num2);
+	printf("After swapping:\n");
+	printf("The value of number 1=%d\n",num1);
+	printf("The value of number 2=%d\n",num2);
+	return 0;
+}
