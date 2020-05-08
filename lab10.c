63def23af52207d251b76f8ab520235f504859f8
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-05-08 19:55:07 +0000

    lab10.c initial commit

--- lab10.c
+++ lab10.c
@@ -1,1 +1,17 @@
-//write your code here
+#include<stdio.h>
+int main()
+{
+	FILE *fp;
+	char c;
+	printf("Data Input\n");
+	fp=fopen("INPUT.txt","w");
+	while((c=getchar())!=EOF)
+		fputc(c,fp);
+	fclose(fp);
+	printf("\nData Output\n");
+	fp=fopen("INPUT.txt","r");
+	while((c=fgetc(fp))!=EOF)
+		printf("%c",c);
+	fclose(fp);
+	return 0;
+}
